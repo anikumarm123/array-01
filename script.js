392083404b753fa23a0260f70361483239b157dd
muthukumar <anikumar2825@gmail.com>
2022-06-14 15:20:03 +0530

    addfile

---
+++ script.js
@@ -0,0 +1,39 @@
+// at() //
+// let Name = ["muthu","mani","ragul","vel"];
+// console.log(Name.at(2));
+// console.log(Name.at(-1));
+// console.log(Name);
+
+// concat() //
+// let text1 = ["welcome","to"]
+// let text2 = ["Fabevy"]
+// let word = text1.concat(text2);
+// console.log(word);
+
+// let tx1 = "well";
+// let tx2 = "come";
+// let wd =tx1.concat(tx2);
+// console.log(wd);
+
+// copywithin() //
+//  let mark = [20,10,50,30,40,90];
+//  console.log(mark.copyWithin(4,2));
+//  console.log(mark);
+
+// fill() //
+// let mark = [10,20,30,40,50];
+// console.log(mark.fill(80));
+// console.log(mark.fill(10,3));
+// console.log(mark);
+
+// forEach //
+// var si_no = [05,02,03,04];
+// function rol_no(no){   
+//     console.log(no); 
+// }
+// si_no.forEach(rol_no);
+
+// includes //
+// let pets = ["dog","cat","rat","rabit"];
+// console.log(pets.includes("cat"));
+// console.log(pets.includes("goat"));
